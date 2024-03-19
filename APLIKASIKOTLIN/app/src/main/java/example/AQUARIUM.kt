1f673c6a77a9b4e6698f5787204f850313393807
Yori03 <yoriprayoga03@gmail.com>
2024-03-19 11:12:36 +0700

    lesson 3 step 2

--- APLIKASIKOTLIN/app/src/main/java/example/AQUARIUM.kt
+++ APLIKASIKOTLIN/app/src/main/java/example/AQUARIUM.kt
@@ -1,2 +1,16 @@
-package example
+package example.myapp
 
+class private{
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Width: $width cm, Length: $length cm, Height: $height cm")
+    }
+}
+
+fun main() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+}
--- APLIKASIKOTLIN/app/src/main/java/example/MAIN.kt
+++ APLIKASIKOTLIN/app/src/main/java/example/MAIN.kt
@@ -1,2 +1,18 @@
-package example
+package example.myapp
 
+class Aquarium(var height: Int = 0) {
+    fun printSize() {
+        println("Aquarium size: Height = $height")
+    }
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+    myAquarium.height = 60
+    myAquarium.printSize()
+}
+
+fun main() {
+    buildAquarium()
+}
